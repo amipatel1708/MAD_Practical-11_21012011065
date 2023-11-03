2b9ef3a21ed84434c8ba0264923c96054425f005
asus <amipatidar383@gmail.com>
2023-11-03 09:14:12 +0530

    Practical 11

--- app/src/main/java/com/example/mad_practical_11_21012011065/Person.kt
+++ app/src/main/java/com/example/mad_practical_11_21012011065/Person.kt
@@ -3,14 +3,15 @@ package com.example.mad_practical_11_21012011065
 import org.json.JSONObject
 import java.io.Serializable
 
-class Person(var id:String,
-             var name:String,
-             var emailId: String,
-             var phoneNo:String,
-             var address:String,
-             var latitude:Double,
-             var longitude:Double):Serializable {
-    constructor(jsonObject: JSONObject):this("","","","","",0.0,0.0) {
+class Person (
+    var id:String,
+    var name:String,
+    var emailId:String,
+    var phoneNo: String,
+    var address :String,
+    var latitude:Double,
+    var longitude:Double):Serializable {
+    constructor(jsonObject:JSONObject):this ("","","","","",90.0,09.8){
         id = jsonObject.getString("id")
         emailId = jsonObject.getString("email")
         phoneNo = jsonObject.getString("phone")
@@ -21,4 +22,5 @@ class Person(var id:String,
         latitude = locationJson.getDouble("lat")
         longitude = locationJson.getDouble("long")
     }
+
 }
